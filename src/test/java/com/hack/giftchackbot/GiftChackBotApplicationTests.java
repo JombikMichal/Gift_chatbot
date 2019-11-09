7a2b2b260a42fe83ab9d332814366a745bd9128a
Lucia Blastakova <lucia.blastakova@elder.cz>
2019-11-09 13:41:47 +0100

    first commjit

---
+++ src/test/java/com/hack/giftchackbot/GiftChackBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.giftchackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftChackBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
