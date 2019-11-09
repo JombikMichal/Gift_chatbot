405ee14a9780a427f8024cf2afb6c3aee309afb3
Lucia Blastakova <lucia.blastakova@elder.cz>
2019-11-09 15:58:24 +0100

    working

---
+++ app/src/main/java/com/hack/config/JpaConfiguration.java
@@ -0,0 +1,15 @@
+package com.hack.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.hack"})
+@EntityScan(basePackages = {"com.hack"})
+public class JpaConfiguration {
+
+
+}
